89d342002839450e6cafa3c6c9fad71f563d7213
CaioVitor1 <caiovitor33@hotmail.com>
2022-08-09 13:24:51 -0300

    first commit

---
+++ src/databases/postgres.js
@@ -0,0 +1,14 @@
+import pg from 'pg';
+import dotenv from 'dotenv';
+dotenv.config();
+const { Pool } = pg;
+
+const connection = new Pool({
+  user: process.env.POSTGRESS_USER,
+  host: process.env.POSTGRESS_HOST,
+  port: process.env.POSTGRESS_PORT,
+  database: process.env.POSTGRESS_DATABASE,
+  password: process.env.POSTGRESS_PASSWORD
+})
+
+export default connection;
